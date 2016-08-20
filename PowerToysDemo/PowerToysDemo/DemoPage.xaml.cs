a54b791e58039aea90b2ae0fa0d1799a10d78364
sturlath <sturlath@TVW106652>
2016-08-20 09:13:25 +0000

    Add project files.

---
+++ PowerToysDemo/PowerToysDemo/DemoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace PowerToysDemo
+{
+    public partial class DemoPage : ContentPage
+    {
+        private DemoViewModel viewModel;
+        public DemoPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new DemoViewModel();
+        }
+    }
+}
---
+++ PowerToysDemo/PowerToysDemo/DemoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using MvvmHelpers;
+
+namespace PowerToysDemo
+{
+    public class DemoViewModel
+    {
+        public ObservableRangeCollection<DemoClass> ThisDoesntWork { get; } = new ObservableRangeCollection<DemoClass>();
+        //public ObservableCollection<DemoClass> ThisWorks { get; } = new ObservableCollection<DemoClass>();
+
+    }
+}
